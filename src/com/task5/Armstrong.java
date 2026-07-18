ca86ca591e938d7c8ad6fdfeb74cecc24bbdc714
vadla-vishwateja <vishwatejachary1107@gmail.com>
2026-07-18 19:16:52 +0530

    armstrong

---
+++ src/com/task5/Armstrong.java
@@ -0,0 +1,25 @@
+package com.task5;
+import java.util.Scanner;
+
+public class Armstrong {
+
+	public static void main(String[] args) {
+			Scanner sc=new Scanner(System.in);
+			System.out.print("Enter a Number : ");
+			int num=sc.nextInt();
+			int original=num ;
+			double sum=0;
+			int TotalDigits=String.valueOf(num).length();
+			while(num>0) {
+				int d=num%10;
+				sum=sum+Math.pow(d, TotalDigits);
+				num=num/10;
+			}
+			if(original==sum) {
+				System.out.println(original+" is an Armstrong Number .");
+			}else {
+				System.out.println(original+" is Not an  Armstrong Number .");
+			}
+	}
+
+}
