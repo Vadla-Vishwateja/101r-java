ffe69233a103c904837357e0758bfaeb21ba2e0d
vadla-vishwateja <vishwatejachary1107@gmail.com>
2026-07-30 21:42:44 +0530

    single inheritance

---
+++ src/com/inheritance/Student.java
@@ -0,0 +1,12 @@
+package com.inheritance;
+
+public class Student extends Person{
+	Student(){
+		System.out.println("Student object created");
+	}
+	public static void main(String[] args) {
+		Student s=new Student();
+
+	}
+
+}
