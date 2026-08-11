7a5160cf8ee3c7602eb2264f19318d88186dea8b
vadla-vishwateja <vishwatejachary1107@gmail.com>
2026-08-11 23:54:05 +0530

    anagram

---
+++ src/com/task13/Anagram.java
@@ -0,0 +1,25 @@
+package com.task13;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Anagram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str1 = sc.nextLine().toLowerCase();
+        String str2 = sc.nextLine().toLowerCase();
+
+        char[] a = str1.toCharArray();
+        char[] b = str2.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        if (Arrays.equals(a, b)) {
+            System.out.println("Anagram");
+        } else {
+            System.out.println("Not Anagram");
+        }
+    }
+}
