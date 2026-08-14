6197be54c39d5ec6556097cbd7570b38a3e4dfdc
vadla-vishwateja <vishwatejachary1107@gmail.com>
2026-08-14 11:46:21 +0530

    pattern

---
+++ src/com/patterns/RevPyramid.java
@@ -0,0 +1,31 @@
+package com.patterns;
+
+import java.util.Scanner;
+
+public class RevPyramid {
+	public static void printPyramid(int num) {
+		if(num<=0) {
+		    System.out.println("Size must be greater than 0.");
+		    return;
+		}
+		for(int i=1;i<=num;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(" ");
+			}
+			for(int k=1;k<=num-i;k++) {
+				System.out.print(" *");
+			}
+			System.out.println();
+		}
+		
+	}
+	public static void main(String []args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter number of rows : ");
+		int n=sc.nextInt();
+		System.out.println("Pyramid : ");
+		printPyramid(n+1);
+}
+
+
+}
