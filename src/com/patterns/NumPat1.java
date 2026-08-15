1af47a72e4427ff661b2f23bde2ccb5060219ff6
vadla-vishwateja <vishwatejachary1107@gmail.com>
2026-08-15 22:30:09 +0530

    patterns

---
+++ src/com/patterns/NumPat1.java
@@ -0,0 +1,25 @@
+package com.patterns;
+
+public class NumPat1 {
+
+	public static void main(String[] args) {
+		int count=1;
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				if(count<10) {
+					System.out.print("0"+count+" ");
+					count++;
+				}
+				else {
+					System.out.print(count+" ");
+					count++;
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+	}
+
+
