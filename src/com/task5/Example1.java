5e7ba3515228f6047faf33f0c2c2e1030798a728
vadla-vishwateja <vishwatejachary1107@gmail.com>
2026-08-22 23:00:24 +0530

    java

---
+++ src/com/task5/Example1.java
@@ -0,0 +1,12 @@
+package com.task5;
+
+public class Example1 {
+	public static void main(String[] args) {
+		int i=1;
+		int sum=0;
+		while(i<=10) {
+			sum=sum+i;
+		}
+		System.out.println(sum);
+	}
+}
