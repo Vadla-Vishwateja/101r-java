168cb4dda69428b53df380a19e5b767fd263e584
vadla-vishwateja <vishwatejachary1107@gmail.com>
2026-09-02 19:43:38 +0530

    java

---
+++ src/com/array/ArrayDouble.java
@@ -0,0 +1,13 @@
+package com.array;
+
+public class ArrayDouble {
+
+	public static void main(String[] args) {
+		double[] a= {10.5,20.75,30.25};
+		for(double k:a) {
+			System.out.print(k+"  ");
+		}
+
+	}
+
+}
