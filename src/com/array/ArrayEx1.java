947a7af3686c996e01c1c59a283421425bb4c568
vadla-vishwateja <vishwatejachary1107@gmail.com>
2026-09-02 19:43:24 +0530

    array

---
+++ src/com/array/ArrayEx1.java
@@ -0,0 +1,16 @@
+package com.array;
+
+public class ArrayEx1 {
+
+	public static void main(String[] args) {
+		int[] arr=new int[3];
+		arr[0]=10;
+		arr[1]=20;
+		arr[2]=30;
+		for(int i=0;i<3;i++) {
+			System.out.print(arr[i]+" ");
+		}
+
+	}
+
+}
