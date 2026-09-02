a4fb837a59df712dd8a568e1044bdacfa7380424
vadla-vishwateja <vishwatejachary1107@gmail.com>
2026-09-02 13:32:26 +0530

    array

---
+++ src/com/array/ArrayBoolean.java
@@ -0,0 +1,14 @@
+package com.array;
+
+public class ArrayBoolean {
+
+	public static void main(String[] args) {
+		boolean[] arr=new boolean[2];
+		arr[0]=true;
+		arr[1]=false;
+		for(int i=0;i<2;i++) {
+			System.out.print(arr[i]+"  ");
+		}
+	}
+
+}
