acb970c470a474c53e8dca5c3e3bb522e5b94d5c
vadla-vishwateja <vishwatejachary1107@gmail.com>
2026-09-02 18:57:53 +0530

    array

---
+++ src/com/array/ArrayChar.java
@@ -0,0 +1,10 @@
+package com.array;
+
+public class ArrayChar {
+	public static void main(String []args) {
+		char[] arr= {'A','B','C','D','E'};
+		for(char j:arr) {
+			System.out.print(j+" ");
+		}
+	}
+}
