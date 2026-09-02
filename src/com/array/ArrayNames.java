e0cb6cee09e7fe01ae226c12e25a66eeb103747f
vadla-vishwateja <vishwatejachary1107@gmail.com>
2026-09-02 19:43:10 +0530

    arrays

---
+++ src/com/array/ArrayNames.java
@@ -0,0 +1,16 @@
+package com.array;
+
+public class ArrayNames {
+
+	public static void main(String[] args) {
+		String[] arr=new String[4];
+		arr[0]="John";
+		arr[1]="Alice";
+		arr[2]="Bob";
+		arr[3]="Emma";
+		
+		for(String i:arr) {
+			System.out.print(i+" ");
+		}
+	}
+}
